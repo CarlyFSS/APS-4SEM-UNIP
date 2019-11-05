98792531c42c8569c55f8d84873768e76205b8a3
Carly Silva <carly.fs.silva@gmail.com>
2019-11-05 14:30:42 -0300

    Changed show result and menu

--- src/app/App.java
+++ src/app/App.java
@@ -18,7 +18,16 @@ public class App {
         // Variables
         int option = 5;
         Scanner scanner = new Scanner(System.in);
-        int[] arr = {};
+        int[] arr1 = {};
+        int[] arr2 = {};
+        int[] arr3 = {};
+        int[] arr4 = {};
+        int[] arr5 = {};
+
+        long[][] times = new long[5][3];
+
+        int max = 100000;
+        int min = 1;
 
         // ----------
 
@@ -29,15 +38,45 @@ public class App {
             switch (option) {
             case 1:
                 clearScreen();
-                arr = showGenMenu(scanner);
+
+                System.out.println("Generating all arrays, please wait...");
+
+                System.out.println("Generating Array 1...");
+                arr1 = generateArray(1000, max, min);
+                System.out.println("Array 1 generated with " + arr1.length + " numbers.");
+
+                System.out.println("Generating Array 2...");
+                arr2 = generateArray(5000, max, min);
+                System.out.println("Array 2 generated with " + arr2.length + " numbers.");
+
+                System.out.println("Generating Array 3...");
+                arr3 = generateArray(10000, max, min);
+                System.out.println("Array 3 generated with " + arr3.length + " numbers.");
+
+                System.out.println("Generating Array 4...");
+                arr4 = generateArray(30000, max, min);
+                System.out.println("Array 4 generated with " + arr4.length + " numbers.");
+
+                System.out.println("Generating Array 5...");
+                arr5 = generateArray(100000, max, min);
+                System.out.println("Array 5 generated with " + arr5.length + " numbers.");
+
                 break;
             case 2:
                 clearScreen();
-                showSortResult(arr, scanner);
+                // showSingleSortResult(arr, scanner);
+
+                System.out.println("Starting all sorts.");
+                times = runAllSortAndStoreTimes(arr1, arr2, arr3, arr4, arr5);
+                System.out.println("Sorting finished.");
+
                 break;
             case 3:
                 clearScreen();
-                showArray(arr);
+                // showArray(arr);
+
+                showAllSortResults(times, scanner);
+
                 break;
             case 4:
                 scanner.close();
@@ -52,7 +91,7 @@ public class App {
 
     // Functions
 
-    public static void showSortResult(int[] arr, Scanner scanner) {
+    public static void showSingleSortResult(int[] arr, Scanner scanner) {
         clearScreen();
         System.out.println("Data amount: " + arr.length);
         System.out.println("\n");
@@ -67,6 +106,109 @@ public class App {
         scanner.nextLine();
     }
 
+    public static long[][] runAllSortAndStoreTimes(int[] arr1, int[] arr2, int[] arr3, int[] arr4, int[] arr5) {
+        long[][] times = new long[5][3];
+
+        // Stores times for array 1
+        System.out.println("\nStarting sorts for Array 1 ...");
+
+        System.out.println("Running Bubble sort on Array 1 ...");
+        times[0][0] = runBubbleSortAndGetTime(arr1);
+        System.out.println("Finished running Bubble sort on Array 1 ...");
+
+        System.out.println("Running Merge sort on Array 1 ...");
+        times[0][1] = runMergeSortAndGetTime(arr1);
+        System.out.println("Finished running Merge sort on Array 1 ...");
+
+        System.out.println("Running Selection sort on Array 1 ...");
+        times[0][2] = runSelectionSortAndGetTime(arr1);
+        System.out.println("Finished running Selection sort on Array 1 ...");
+
+        // Stores times for array 2
+        System.out.println("\nStarting sorts for Array 2 ...");
+
+        System.out.println("Running Bubble sort on Array 2 ...");
+        times[1][0] = runBubbleSortAndGetTime(arr2);
+        System.out.println("Finished running Bubble sort on Array 2 ...");
+
+        System.out.println("Running Merge sort on Array 2 ...");
+        times[1][1] = runMergeSortAndGetTime(arr2);
+        System.out.println("Finished running Merge sort on Array 2 ...");
+
+        System.out.println("Running Selection sort on Array 2 ...");
+        times[1][2] = runSelectionSortAndGetTime(arr2);
+        System.out.println("Finished running Selection sort on Array 2 ...");
+
+        // Stores times for array 3
+        System.out.println("\nStarting sorts for Array 3 ...");
+
+        System.out.println("Running Bubble sort on Array 3 ...");
+        times[2][0] = runBubbleSortAndGetTime(arr3);
+        System.out.println("Finished running Bubble sort on Array 3 ...");
+
+        System.out.println("Running Merge sort on Array 3 ...");
+        times[2][1] = runMergeSortAndGetTime(arr3);
+        System.out.println("Finished running Merge sort on Array 3 ...");
+
+        System.out.println("Running Selection sort on Array 3 ...");
+        times[2][2] = runSelectionSortAndGetTime(arr3);
+        System.out.println("Finished running Selection sort on Array 3 ...");
+
+        // Stores times for array 4
+        System.out.println("\nStarting sorts for Array 4 ...");
+
+        System.out.println("Running Bubble sort on Array 4 ...");
+        times[3][0] = runBubbleSortAndGetTime(arr4);
+        System.out.println("Finished running Bubble sort on Array 4 ...");
+
+        System.out.println("Running Merge sort on Array 4 ...");
+        times[3][1] = runMergeSortAndGetTime(arr4);
+        System.out.println("Finished running Merge sort on Array 4 ...");
+
+        System.out.println("Running Selection sort on Array 4 ...");
+        times[3][2] = runSelectionSortAndGetTime(arr4);
+        System.out.println("Finished running Selection sort on Array 4 ...");
+
+        // Stores times for array 5
+        System.out.println("\nStarting sorts for Array 5 ...");
+
+        System.out.println("Running Bubble sort on Array 5 ...");
+        times[4][0] = runBubbleSortAndGetTime(arr5);
+        System.out.println("Finished running Bubble sort on Array 5 ...");
+
+        System.out.println("Running Merge sort on Array 5 ...");
+        times[4][1] = runMergeSortAndGetTime(arr5);
+        System.out.println("Finished running Merge sort on Array 5 ...");
+
+        System.out.println("Running Selection sort on Array 5 ...");
+        times[4][2] = runSelectionSortAndGetTime(arr5);
+        System.out.println("Finished running Selection sort on Array 5 ...");
+
+        return times;
+    }
+
+    public static void showAllSortResults(long[][] times, Scanner scanner) {
+        String leftAlignFormat = "|%-15s | %-9d | %-9d | %-11d | %-12d | %-13d | %n";
+
+        System.out.format("+-------------------------------------------------------------------------------------+%n");
+        System.out.format("|Method          |   10000   |   100000  |   1000000   |   10000000   |   100000000   |%n");
+        System.out.format("+----------------|-----------|-----------|-------------|--------------|---------------+%n");
+
+        System.out.format(leftAlignFormat, "Bubble Sort", times[0][0], times[1][0], times[2][0], times[3][0],
+                times[4][0]);
+        System.out.format(leftAlignFormat, "Merge Sort", times[0][1], times[1][1], times[2][1], times[3][1],
+                times[4][1]);
+        System.out.format(leftAlignFormat, "Selection Sort", times[0][2], times[1][2], times[2][2], times[3][2],
+                times[4][2]);
+
+        System.out.format("+-------------------------------------------------------------------------------------+%n");
+
+        System.out.println("\n\n");
+        System.out.println("Press any key to continue...");
+        scanner.nextLine();
+        scanner.nextLine();
+    }
+
     public static long runBubbleSortAndGetTime(int[] arr) {
         TimeCounter timeCounter = new TimeCounter();
 
@@ -74,7 +216,7 @@ public class App {
         arr = BubbleSort.sort(arr);
         timeCounter.stopCounter();
 
-        return timeCounter.calculateElapsedTimeInSeconds();
+        return timeCounter.calculateElapsedTimeInMillis();
     }
 
     public static long runSelectionSortAndGetTime(int[] arr) {
@@ -84,7 +226,7 @@ public class App {
         // arr = SelectionSort.sort(arr);
         timeCounter.stopCounter();
 
-        return timeCounter.calculateElapsedTimeInSeconds();
+        return timeCounter.calculateElapsedTimeInMillis();
     }
 
     public static long runMergeSortAndGetTime(int[] arr) {
@@ -94,14 +236,14 @@ public class App {
         // arr = MergeSort.sort(arr);
         timeCounter.stopCounter();
 
-        return timeCounter.calculateElapsedTimeInSeconds();
+        return timeCounter.calculateElapsedTimeInMillis();
     }
 
     public static void showMainMenu() {
         System.out.println("\n\nWhat do you want to do?");
         System.out.println("1 - Generate number array");
         System.out.println("2 - Test sort algorithms");
-        System.out.println("3 - Show Array");
+        System.out.println("3 - Show results");
         System.out.println("4 - Exit");
     }
 
@@ -191,6 +333,14 @@ public class App {
         System.out.print("\n\n Array generated!");
 
         clearScreen();
+        return arr;
+    }
+
+    public static int[] generateArray(int size, int max, int min) {
+        int[] arr = null;
+
+        arr = generateNumberArray(size, max, min);
+
         return arr;
     }
 
